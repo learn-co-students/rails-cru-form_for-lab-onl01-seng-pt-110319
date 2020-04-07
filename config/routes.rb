3072ff703571f5c4a14e2b2aff44e36d414b290d
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-04-07 10:20:44 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'artists/:index'
+
+  get 'artists/:show'
+
+  get 'artists/:new'
+
+  get 'artists/:create'
+
+  get 'artists/:edit'
+
+  get 'artists/:update'
+
+  resources :artists
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
